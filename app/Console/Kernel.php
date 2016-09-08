840e591643745757fc51dfd1e82b31ca8ada30e2
tonydt <tonydt@yahoo-inc.com>
2016-09-09 01:28:51 +0800

    try scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('command:collect')
-            ->cron('23 1 * * * *')
+            ->cron('31 1 * * * *')
             ->appendOutputTo(storage_path('logs/schedule.log'));
     }
 }
